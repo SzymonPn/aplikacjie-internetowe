f25f28f38ee6d2e4a5696eb1537150d9fbb9e0c9
SzymonPn <szymonpniaczek@gmail.com>
2023-10-05 08:13:08 +0200

    cookie

--- cookie.php
+++ cookie.php
@@ -1,4 +1,10 @@
 <?php 
-// Set-cookie: NAME=WARTOSC;[expires=DATA;][path=ŚCIEŻKA;][domain=NAZWA_DOMENY;][secure]
 setcookie("imie","john",time()+24*60*60);
+$cookie_name="imie";
+if(!isset($_COOKIE[$cookie_name])) {
+    echo "Cookie named '" . $cookie_name . "' is not set!";
+  } else {
+    echo "Cookie '" . $cookie_name . "' is set!<br>";
+    echo "Value is: " . $_COOKIE[$cookie_name];
+  }
 ?>
