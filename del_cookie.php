faea3faa20835b340a57a839ad0d8a206b1d2940
SzymonPn <szymonpniaczek@gmail.com>
2023-10-05 08:39:45 +0200

    cookie

--- del_cookie.php
+++ del_cookie.php
@@ -1,11 +1,10 @@
-<?php 
-setcookie("imie","john",time()+24*60*60);
-$cookie_name="imie";
+<?php
+setcookie("user","",time()-3600);
+$cookie_name="user";
 if(!isset($_COOKIE[$cookie_name])) {
     echo "Cookie named '" . $cookie_name . "' is not set!";
   } else {
     echo "Cookie '" . $cookie_name . "' is set!<br>";
     echo "Value is: " . $_COOKIE[$cookie_name];
 };
-
 ?>
