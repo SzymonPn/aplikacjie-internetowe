c5af0ec4bef8977933876f1687bc806e1d1af35a
SzymonPn <szymonpniaczek@gmail.com>
2023-10-05 09:01:18 +0200

    delcar

---
+++ auto_cookie.php
@@ -0,0 +1,14 @@
+<?php
+function checkCookie($cookie_name){
+    if(!isset($_COOKIE[$cookie_name])) {
+        echo "Cookie named '" . $cookie_name . "' is not set!";
+      } else {
+        echo "Cookie '" . $cookie_name . "' is set!<br>";
+        echo "Value is: " . $_COOKIE[$cookie_name];
+    };
+};
+setcookie("auto","cupra",time()+24*60*60*2);
+checkCookie("auto");
+// setcookie("auto","cupra",time()-24*60*60*2);
+// checkCookie("auto");
+?>
