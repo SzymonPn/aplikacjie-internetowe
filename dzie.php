e4fe77a75938911c0551de97c025baf45f3c811d
SzymonPn <szymonpniaczek@gmail.com>
2023-10-18 13:44:59 +0200

    moddo

---
+++ dzie.php
@@ -0,0 +1,13 @@
+<?php
+class Fruit {
+  public $name;
+  public $color;
+  public function __construct($name, $color) {
+    $this->name = $name;
+    $this->color = $color;
+  }
+  public function intro() {
+    echo "The fruit is {$this->name} and the color is {$this->color}.";
+  }
+}
+?>
--- moddo.php
+++ moddo.php
@@ -7,6 +7,6 @@ class Fruit1 {
 
 $mango = new Fruit1();
 $mango->name = 'Mango'; // OK
-$mango->color = 'Yellow'; // ERROR
-$mango->weight = '300'; // ERROR
+//$mango->color = 'Yellow'; // ERROR
+//$mango->weight = '300'; // ERROR
 ?>
