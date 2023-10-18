ad6ed420e2976948cb297acef57345e3e2b9920a
SzymonPn <szymonpniaczek@gmail.com>
2023-10-18 13:23:17 +0200

    desc

---
+++ desc.php
@@ -0,0 +1,15 @@
+<?php
+class Fruit {
+  public $name;
+  public $color;
+
+  function __construct($name) {
+    $this->name = $name;
+  }
+  function __destruct() {
+    echo "The fruit is {$this->name}.";
+  }
+}
+
+$apple = new Fruit("Apple");
+?>
