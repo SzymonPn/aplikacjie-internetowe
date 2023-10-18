23cf889907b563f649b2b99a19728e7901c35dd2
SzymonPn <szymonpniaczek@gmail.com>
2023-10-18 13:10:23 +0200

    konst

--- klasa.php
+++ klasa.php
@@ -1,27 +1,27 @@
 <?php 
-class Fruit{
-    public $name;
-    public $color;
+// class Fruit{
+//     public $name;
+//     public $color;
 
-    function setName($name){
-        $this->name=$name;
-    }
+//     function setName($name){
+//         $this->name=$name;
+//     }
 
-    function getName(){
-        return $this->name;
-    }
-}
-$apple = new Fruit();
-$banana = new Fruit();
-$orange= new Fruit();
+//     function getName(){
+//         return $this->name;
+//     }
+// }
+// $apple = new Fruit();
+// $banana = new Fruit();
+// $orange= new Fruit();
 
-$apple->setName('Apple');
-$banana->setName('Banana');
-$orange->setName("Orange");
+// $apple->setName('Apple');
+// $banana->setName('Banana');
+// $orange->setName("Orange");
 
-echo $apple->getName();
-echo "<br>";
-echo $banana->getName();
-echo "<br>";
-echo $orange->getName();
+// echo $apple->getName();
+// echo "<br>";
+// echo $banana->getName();
+// echo "<br>";
+// echo $orange->getName();
 ?>
---
+++ konst.php
@@ -0,0 +1,22 @@
+<?php
+class Fruit {
+  public $name;
+  public $color;
+
+  function __construct($name,$color) {
+    $this->name = $name;
+    $this->color=$color;
+  }
+  function getName() {
+    return $this->name;
+  }
+  function getColor() {
+    return $this->color;
+  }
+}
+
+$apple = new Fruit("Apple","Red");
+echo $apple->getName();
+echo "<br>";
+echo $apple->getColor();
+?>
