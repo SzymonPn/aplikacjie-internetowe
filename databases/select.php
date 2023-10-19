a19de2843524b72de74569ac8209f0c1ac59a737
SzymonPn <szymonpniaczek@gmail.com>
2023-10-19 09:53:18 +0200

    select

--- databases/select.php
+++ databases/select.php
@@ -11,14 +11,23 @@ if (!$conn) {
 echo "Connected successfully<br>";
 $sql = "SELECT id, firstname, lastname FROM MyGuests";
 $result = mysqli_query($conn, $sql);
-
+echo "<style>
+table,td,th{
+    border: 1px solid;
+    border-collapse:collapse;
+}
+</style>";
+echo "<table>\n";
+echo "<tr><td>id</td><td>name</td><td>surname</td></tr>\n";
 if (mysqli_num_rows($result) > 0) {
   while($row = mysqli_fetch_assoc($result)) {
-    echo "id: ". $row["id"]." - Name: ".$row["firstname"]." ". $row["lastname"]."<br>";
+    //echo "id: ". $row["id"]." - Name: ".$row["firstname"]." ". $row["lastname"]."<br>";
+    echo "<tr><td>".$row["id"]."</td><td>".$row["firstname"]."</td><td>".$row["lastname"]."</td></tr>\n";
   }
 } else {
   echo "0 results";
 }
+echo "</table>\n";
 
 mysqli_close($conn);
 ?>
