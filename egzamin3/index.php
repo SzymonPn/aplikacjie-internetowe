5991ce3de3e9cedb8ce82fb45cbbe180523b959a
SzymonPn <szymonpniaczek@gmail.com>
2023-11-15 14:12:52 +0100

    inf3

---
+++ egzamin3/index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="styl.css">
+    <title>Hurtownia szkolna</title>
+
+</head>
+<body>
+    <div class="baner">
+        <h1>Hurtownia z najlepszymi cenami</h1>
+    </div>
+    <div class="lewy">
+        <h2>Nasze ceny</h2>
+        <p>Tabela składająca się z czterech wierszy i dwóch kolumn o zawartości generowanej skryptem 1</p>
+        <?php 
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "sklep";
+            $conn = mysqli_connect($servername, $username, $password,$dbname);
+            
+            if (!$conn) {
+              die("Connection failed: " . mysqli_connect_error());
+            }
+            $sql="select nazwa, cena from towary limit 4;";
+            $wynik=mysqli_query($conn,$sql);
+            echo "<table>";
+            if(mysqli_num_rows($wynik)>0){
+                while($row=mysqli_fetch_assoc($wynik)){
+                    echo "<tr><td>$row[nazwa]</td><td>$row[cena]</td><tr>";
+                };
+            };
+            echo "</table>";
+            mysqli_close($conn);
+        ?>
+    </div>
+    <div class="srodkowy">
+        <h2>Koszt zakupów</h2>
+        <form action="index.php" method="post">
+        wybierz artykuł:<br>
+        <select name="produkt">
+            <option value="Zeszyt 60 kartek" >Zeszyt 60 kartek</option>
+            <option value="Zeszyt 32 kartki" >Zeszyt 32 kartki</option>
+            <option value="Cyrkiel">Cyrkiel</option>
+            <option value="Linijka 30 cm">Linijka 30 cm</option>
+        </select><br>
+        <label>liczba sztuk: <input type="number" name="liczbaSztuk"></label><br>
+        <input type="submit" value="OBLICZ"> 
+        </form>
+        <?php 
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "sklep";
+            $conn = mysqli_connect($servername, $username, $password,$dbname);
+            if(isset($_POST["produkt"]) && isset($_POST["liczbaSztuk"])){
+                $nazwa=$_POST["produkt"];
+                $ilosc=$_POST["liczbaSztuk"];
+                if (!$conn) {
+                  die("Connection failed: " . mysqli_connect_error());
+                }
+                $slq="select cena from towary where nazwa='$nazwa';";
+                if($wynik1=mysqli_query($conn,$sql)){
+                    $wiersz=mysqli_fetch_row($wynik1);
+
+                    echo "wartość zakupów:". $wiersz[0]*$ilosc;
+                }
+            }
+            
+        ?>
+    </div>
+    <div class="prawy">
+        <h2>Kontakt</h2>
+        <img src="zakupy.png" alt="hurtownia">
+        <p>email: <a href="mailto:hurt@poczta2.pl">hurt@poczta2.pl</a></p>
+    </div>
+    <div class="stopka">
+    <h4>Witrynę wykonał: Szymon Pniaczek</h4>
+    </div>
+</body>
+</html>
